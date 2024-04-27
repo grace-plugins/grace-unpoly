e29550bae3a8d86de0300b48113d43d2403d6972
Michael Yan <rain@rainboyan.com>
2024-04-27 18:35:04 +0800

    Update Unpoly plugin
    Add plugin descriptor

--- src/main/groovy/org/graceframework/plugins/unpoly/UnpolyGrailsPlugin.groovy
+++ src/main/groovy/org/graceframework/plugins/unpoly/UnpolyGrailsPlugin.groovy
@@ -1,22 +1,34 @@
-package org.graceframework.plugins
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.graceframework.plugins.unpoly
 
 import grails.plugins.*
 
 class UnpolyGrailsPlugin extends Plugin {
 
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2022.2.4 > *"
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-        "app/views/error.gsp"
-    ]
+    def version = '0.0.1'
+    def grailsVersion = "3.0.0 > *"
+    def dependsOn = [controllers: grailsVersion]
+    def observe = ['controllers']
 
-    // TODO Fill in these fields
-    def title = "Grace Unpoly" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
+    def title = "Grace Unpoly Plugin"
+    def author = "Michael Yan"
+    def authorEmail = "rain@rainboyan.com"
     def description = '''\
-Brief summary/description of the plugin.
+Grace Plugin for using Grace app with Unpoly.
 '''
     def profiles = ['web']
 
@@ -26,45 +38,15 @@ Brief summary/description of the plugin.
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+    def license = "APACHE"
 
     // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Michael Yan", email: "rain@rainboyan.com" ]]
+    def developers = [ [id: "rainboyan", name: "Michael Yan", email: "rain@rainboyan.com" ]]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "GitHub", url: "http://github.com/grace-plugins/my-plugin/issues" ]
+    def issueManagement = [ system: "GitHub", url: "https://github.com/grace-plugins/grace-unpoly/issues" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://github.com/grace-plugins/my-plugin" ]
+    def scm = [ url: "https://github.com/grace-plugins/grace-unpoly" ]
 
-    Closure doWithSpring() { {->
-            // TODO Implement runtime spring config (optional)
-        }
-    }
-
-    void doWithDynamicMethods() {
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    void doWithApplicationContext() {
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    void onChange(Map<String, Object> event) {
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    void onConfigChange(Map<String, Object> event) {
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    void onShutdown(Map<String, Object> event) {
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
 }
