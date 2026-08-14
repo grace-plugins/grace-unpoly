cac0dc1ae719d6b93fb8097e68a66b06793607db
Michael Yan <rain@rainboyan.com>
2026-08-15 02:03:29 +0800

    Release v0.5.0

--- plugin/src/main/groovy/org/graceframework/plugins/unpoly/UnpolyGrailsPlugin.groovy
+++ plugin/src/main/groovy/org/graceframework/plugins/unpoly/UnpolyGrailsPlugin.groovy
@@ -28,7 +28,7 @@ import grails.plugins.*
 @CompileStatic
 class UnpolyGrailsPlugin extends Plugin {
 
-    def version = '0.5.0-SNAPSHOT'
+    def version = '0.5.0'
     def grailsVersion = "3.0.0 > *"
     def dependsOn = [controllers: grailsVersion]
     def observe = ['controllers']
